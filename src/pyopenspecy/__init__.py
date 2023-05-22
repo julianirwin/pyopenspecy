37bd58febc294022a766fae5d22ed67e949a35a5
Julian Irwin <julian.irwin@gmail.com>
2023-05-22 09:26:40 -0500

    return metadata and spectrum

--- src/pyopenspecy/__init__.py
+++ src/pyopenspecy/__init__.py
@@ -1,3 +1,3 @@
 from .__version__ import *
 
-from pyopenspecy.pyopenspecy import random_ftir_spectrum, random_raman_spectrum, random_raman_metadata, random_ftir_metadata
+from pyopenspecy.pyopenspecy import random_ftir_spectrum, random_raman_spectrum, random_raman_metadata, random_ftir_metadata, ftir_spectrum_by_id, raman_spectrum_by_id
--- src/pyopenspecy/pyopenspecy.py
+++ src/pyopenspecy/pyopenspecy.py
@@ -2,19 +2,19 @@ from importlib.resources import files
 import pandas as pd
 import pyopenspecy
 
-def random_raman_spectrum():
+def random_raman_spectrum() -> tuple[pd.DataFrame, dict]:
     metadata = random_raman_metadata()
     df = _raman_data_df()
-    return df[df.sample_name == metadata["sample_name"]]
+    return df[df.sample_name == metadata["sample_name"]], metadata
 
 
-def random_ftir_spectrum():
+def random_ftir_spectrum() -> tuple[pd.DataFrame, dict]:
     metadata = random_ftir_metadata()
     df = _ftir_data_df()
-    return df[df.sample_name == metadata["sample_name"]]
+    return df[df.sample_name == metadata["sample_name"]], metadata
 
 
-def random_ftir_metadata():
+def random_ftir_metadata() -> dict:
     metadata_row = _ftir_metadata_df().sample(n=1)
     return {
         "sample_name": metadata_row.sample_name.iloc[0],
@@ -24,7 +24,7 @@ def random_ftir_metadata():
     }
 
 
-def random_raman_metadata():
+def random_raman_metadata() -> dict:
     metadata_row = _raman_metadata_df().sample(n=1)
     return {
         "sample_name": metadata_row.sample_name.iloc[0],
@@ -34,27 +34,31 @@ def random_raman_metadata():
     }
 
 
-def _raman_data_df():
-    data_filepath = files(pyopenspecy) / "data/raman_library.csv"
+def _raman_data_df() -> pd.DataFrame:
+    data_filepath = str(files(pyopenspecy) / "data/raman_library.csv")
     return pd.read_csv(data_filepath)
 
 
-def _ftir_data_df():
-    metadata_filepath = files(pyopenspecy) / "data/ftir_library.csv"
+def _ftir_data_df() -> pd.DataFrame:
+    metadata_filepath = str(files(pyopenspecy) / "data/ftir_library.csv")
     return pd.read_csv(metadata_filepath)
 
 
-def _raman_metadata_df():
-    metadata_filepath = files(pyopenspecy) / "data/raman_metadata.csv"
+def _raman_metadata_df() -> pd.DataFrame:
+    metadata_filepath = str(files(pyopenspecy) / "data/raman_metadata.csv")
     return pd.read_csv(metadata_filepath)
 
 
-def _ftir_metadata_df():
-    metadata_filepath = files(pyopenspecy) / "data/ftir_metadata.csv"
+def _ftir_metadata_df() -> pd.DataFrame:
+    metadata_filepath = str(files(pyopenspecy) / "data/ftir_metadata.csv")
     return pd.read_csv(metadata_filepath)
 
 
-def raman_spectrum_by_id(id):
+def raman_spectrum_by_id(id: int) -> pd.DataFrame:
+    df = _raman_data_df()
+    return df[df.sample_name == id]
+
+def ftir_spectrum_by_id(id: int) -> pd.DataFrame:
     df = _raman_data_df()
     return df[df.sample_name == id]
 
